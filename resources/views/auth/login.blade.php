d0e28938f22a76d868166572c48c2696bf5b2c7d
Farrosyehan Muhammad Ramdhan <farrosyehanidnsolo@gmail.com>
2024-05-29 08:53:18 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,41 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
+<div class="page-content page-auth">
+    <div class="section-store-auth" data-aos="fade-up">
+        <div class="container">
+            <div class="row align-items-center row-login">
+                <div class="col-lg-6 text-center">
+                    <img src="/images/imagelogin.png" alt="" class="w-50 mb-4 mb-lg-none" />
+                </div>
+                <div class="col-lg-5">
+                    <h2>
+                        Shopping for basic needs, <br />
+                        becomes easier
+                    </h2>
+                    <form action="" class="mt-3">
+                        <div class="form group">
+                            <label for="">Email Address</label>
+                            <input type="email" class="form-control w-75" />
+                        </div>
+                        <div class="form group">
+                            <label for="">Password</label>
+                            <input type="password" class="form-control w-75" />
+                        </div>
+                        <a href="/dashboard.html" class="btn btn-warning btn-block w-75 mt-4 text-white">
+                            Sign In
+                        </a>
+                        <a href="/register.html" class="btn btn-signup btn-block w-75 mt-4">
+                            Sign Up
+                        </a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container" style="display: none;">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -18,9 +52,9 @@
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -32,9 +66,9 @@
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -58,9 +92,9 @@
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
                                 @endif
                             </div>
                         </div>
