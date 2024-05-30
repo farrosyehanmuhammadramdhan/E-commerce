bc138a2908d7e440bd46d63b2ae601c94e35fccb
Farrosyehan Muhammad Ramdhan <farrosyehanidnsolo@gmail.com>
2024-05-30 09:29:31 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,19 @@ Route::get('/categories', [App\Http\Controllers\CategoryController::class, 'inde
 Route::get('/details/{id}', [App\Http\Controllers\DetailController::class, 'index'])->name('detail');
 Route::get('/carts', [App\Http\Controllers\CartController::class, 'index'])->name('cart');
 Route::get('/success', [App\Http\Controllers\CartController::class, 'success'])->name('success');
+
+
 Route::get('/register/success', [App\Http\Controllers\Auth\RegisterController::class, 'success'])->name('register-success');
+
+
 Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
+
+
 Route::get('/dashboard/products', [App\Http\Controllers\DashboardProductsController::class, 'index'])->name('dashboard-product');
+Route::get('/dashboard/products/create', [App\Http\Controllers\DashboardProductsController::class, 'create'])->name('dashboard-products-create');
+Route::get('/dashboard/products/{id}', [App\Http\Controllers\DashboardProductsController::class, 'details'])->name('dashboard-products-details');
+
+Route::get('/dashboard/transactions', [App\Http\Controllers\DashboardTransactionsController::class, 'index'])->name('dashboard-transactions');
+
+
 Auth::routes();
