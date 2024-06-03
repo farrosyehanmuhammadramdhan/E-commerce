9a1bc5e5ba1102c0cbcfa0416dc966a339d1ff44
Farrosyehan Muhammad Ramdhan <farrosyehanidnsolo@gmail.com>
2024-06-03 08:20:29 +0700

    Add files via upload

---
+++ app/Http/Controllers/DashboardTransactionsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardTransactionsController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    
+     public function index()
+     {
+         return view('pages.dashboard-transactions');
+     }
+
+     public function details()
+     {
+         return view('pages.dashboard-transactions-details');
+     }
+}
